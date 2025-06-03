67d94cf3f2d33386b58a20807de05584abf07c91
18MIGUEL2003 <agudelomiguel04@gmail.com>
2025-06-03 11:03:32 -0500

    Agrego el ejercicio 4 completo - Formulario html enviado a formulario.php para imprimir usando POST - Se agrega carpeta con datos importantes de PHP

--- Mes_1/Dia_3/ejercicio4.php
+++ Mes_1/Dia_3/ejercicio4.php
@@ -6,13 +6,13 @@
     <title>Formulario simple</title>
 </head>
 <body>
-<form action="/ejercicio4.php">
+<form action="formulario.php" method="POST">
   <label for="nombre">Nombre:</label><br>
-  <input type="text" id="nombre" name="nombre" value="Miguel"><br>
+  <input type="text" id="nombre" name="nombre" value="Miguel" required><br>
   <label for="edad">Edad:</label><br>
   <input type="number" id="edad" name="edad" value=21><br>
   <label for="comentario">Comentario:</label><br>
-  <input type="text" id="comentario" name="comentario" value=""><br><br>
+  <input type="text" id="comentario" name="comentario" value="" required><br><br>
   <input type="submit" value="Submit">
 </form>
 </body>
---
+++ Mes_1/Dia_3/formulario.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Tu nombre es: <?php echo $_POST["nombre"]; ?><br>
+Y tienes: <?php echo $_POST["edad"]; ?> años de edad <br>
+Comentarios: <?php echo $_POST["comentario"]; ?>
+
+</body>
+</html>
